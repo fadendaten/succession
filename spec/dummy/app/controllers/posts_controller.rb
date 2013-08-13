b93e4587be3c79c7c08e6b82edeba83d842b4d9a
Felix Langenegger <f.langenegger@fadendaten.ch>
2013-08-13 14:17:01 +0200

    New spec file

---
+++ spec/dummy/app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.sort_by{|p| -p.rank}
+  end
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'd2ad7860eb46db3ac514762380633e0581482428dc173c9135b2c7ed746eb2a8d867d544767562f1b88eec062e703a9d3d3cc8dfe349903a127c13be0b4a0837'
