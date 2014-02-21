e295ef900faccb44ecb1f807751a71f2ea2e3cc3
simplay <michael.single@students.unibe.ch>
2014-02-21 16:17:00 +0100

    Issue #1: Fixed rank update behavior
    Instead incrementing the rank of a target entity just be
    incrementing/decrementing its rank by 1, we now swap our target's rank
    with its corresponding relative entity if existent. Such an approach of
    a swap mechanism fixes the issue of having entities with the same rank
    present. furthermore, there was another issue present: it might happen,
    that previousely two neighboring entities had a rank distance of a value
    bigger than 1. Thus, someone who wanted to modify a target's rank had
    to invoke several times the same rank update action over and over again
    in order to swap target's rank with its neighbor rank.

--- app/models/succession/entity.rb
+++ app/models/succession/entity.rb
@@ -1,18 +1,48 @@
 module Succession
   class Entity < ActiveRecord::Base
     belongs_to :parent, polymorphic: true
-
     attr_accessible :parent_id, :parent_type, :rank
-
+    
+    # swap rank of self with successor entity if existent.
     def up
-      self.rank += 1
-      self.save
+      update_relative(:+)
     end
 
-
+    # swap rank of self with ancestor entity if existent.
     def down
-      self.rank -= 1
-      self.save
+      update_relative(:-)
     end
+    
+    private
+    
+    # either increment or decrement rank of current entity
+    # depending on provided input argument 'op'.
+    # argument op is either equal :+ or :-
+    # useful in order to just have one relative rank update method
+    def update_relative op
+      sign = 0.send(op, 1)
+      relative = relative op
+        
+      unless relative.blank?
+        self.rank += 1*sign
+        self.save
+        relative.rank -= 1*sign
+        relative.save
+      end
+    end
+    
+    def relative(op)
+      type_entities = self.class.ordered_rank_of parent_type
+      relative_rank = self.rank.send(op, 1)
+      type_entities.where(:rank => relative_rank).first
+    end
+    
+    # get all entities of given type provided as string
+    # this method is a useful for swaping all entities
+    def self.ordered_rank_of type
+      type_entities = Succession::Entity.where(:parent_type => type)
+      type_entities.order('rank DESC')
+    end
+    
   end
 end
